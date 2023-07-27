44710e8f47a19fcacdf8321cbf6750d8218e2711
Julian Lopez <64julianlopez@gmail.com>
2023-07-27 04:34:24 +0000

    added txtfile selection, worked on home folder bug

--- aws-rakugo-downloader.py
+++ aws-rakugo-downloader.py
@@ -1,9 +1,11 @@
 import os
 from yt_dlp import YoutubeDL
+import argparse
+#import boto3
 
 def read_txt_urls(file_name):
     #return a list of urls from each line of a text filec in the home directory
-    with open( os.path.expanduser(f'./{file_name}'), 'r') as f:
+    with open(os.path.expanduser(file_name), 'r') as f:
         list_of_urls = f.readlines()
     for i in range(len(list_of_urls)):
         list_of_urls[i] = list_of_urls[i].strip()
@@ -14,7 +16,7 @@ def download_videos(list_of_urls):
         website_name = url.split('/')[2].replace('www.', '').split('.')[0].lower()
         print(website_name)
         if not os.path.exists(f'~/downloaded-videos/{website_name}'):
-            os.makedirs(f'~/downloaded-videos/{website_name}')
+            os.makedirs(os.path.expanduser(f'~/downloaded-videos/{website_name}'))
         ydl_opts = {
             'outtmpl': f'~/downloaded-videos/{website_name}/%(title)s.%(ext)s'
         }
@@ -29,9 +31,14 @@ def download_videos(list_of_urls):
 #def upload_folder_contents_to_AWS_S3()
 
 
-def main():
-    print(read_txt_urls("test.txt"))
-    download_videos(read_txt_urls("test.txt"))
+
+def main(args):
+    print(read_txt_urls(args.txtfile))
+    download_videos(read_txt_urls(args.txtfile))
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Download videos from URLs in a text file')
+    parser.add_argument('--txtfile', metavar='txtfile', type=str, help='path to the text file containing URLs')
+    #parser.add_argument('--outputdir', metavar='outputdir', type=str, help='path to the output directory')
+    args = parser.parse_args()
+    main(args)
